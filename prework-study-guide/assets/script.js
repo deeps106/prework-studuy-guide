c730a0c53b6458d5d5d7e8bd2be8f0fee86489ac
deeps106 <d.singhranger@yahoo.co.uk>
2022-11-20 12:51:29 +0000

    changes made to script.js array and function cmds added

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -4,22 +4,22 @@ var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 //javascript array for topics and each listed and now using a function tool
 
 function listTopics() {
-for (var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
-}
+    for (var x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
 }
 function selectTopic() {
-if (randomTopic === 'HTML') {
- console.log("Let's study HTML!");
-} else if (randomTopic === 'CSS') {
- console.log("Let's study CSS!");
-} else if (randomTopic === 'Git') {
- console.log("Let's study Git!");
-} else if (randomTopic === 'JavaScript') {
- console.log("Let's study JavaScript!");
-} else {
- console.log('Please try again!');
-} 
+    if (randomTopic === 'HTML') {
+        console.log("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+        console.log("Let's study CSS!");
+    } else if (randomTopic === 'Git') {
+        console.log("Let's study Git!");
+    } else if (randomTopic === 'JavaScript') {
+        console.log("Let's study JavaScript!");
+    } else {
+        console.log('Please try again!');
+    }
 }
 
 console.log('Here are the topics we learned through Prework:');
